1f75aeeb8e88974d7ee7c7cbcef87e1864f8f227
Cristhian Guzman <crguzman@jamasoftware.com>
2023-03-30 09:56:17 -0600

    application server and database server preflights added

---
+++ application-server.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+os=$(uname -s)
+arch=$(uname -m)
+current_version="v0.59.0"
+url="https://github.com/replicatedhq/troubleshoot/releases/download"
+
+#this file name can be replaced by an argument, but for test purposes is hard coded.
+supportbundle_yaml="host-preflight.yml"
+
+
+cleanup(){
+	if [ -f /tmp/LICENSE ]
+	then
+		rm /tmp/LICENSE
+	fi
+	if [ -f /tmp/README.md ]
+	then
+		rm /tmp/README.md
+	fi
+	if [ -f /tmp/key.pub ]
+	then
+		rm /tmp/key.pub
+	fi
+	if [ -f /tmp/troubleshoot-sbom.tgz ]
+	then
+		rm /tmp/troubleshoot-sbom.tgz*
+	fi
+	if [ -f /tmp/preflight ]
+	then
+		rm /tmp/preflight*
+	fi
+	if [ -f /tmp/support-bundle ]
+	then
+		rm /tmp/support-bundle*
+	fi
+}
+
+
+supportbundle_linux_os_x86_64(){
+	echo "Replicated Support Bundle loading for Linux x86_64..."
+	wget -q -P /tmp $url/$current_version/support-bundle_linux_amd64.tar.gz
+	tar zxf /tmp/support-bundle_linux_amd64.tar.gz -C /tmp
+	cp $supportbundle_yaml /tmp/support-bundle.yaml
+	/tmp/support-bundle /tmp/support-bundle.yaml
+	cleanup
+}
+
+supportbundle_linux_os_arm64(){
+	echo "Replicated Support Bundle loading for Linux arm64..."
+	wget -q -P /tmp $url/$current_version/support-bundle_linux_arm64.tar.gz
+	tar zxf /tmp/support-bundle_linux_arm64.tar.gz -C /tmp
+	cp $supportbundle_yaml /tmp/support-bundle.yaml
+	/tmp/support-bundle /tmp/support-bundle.yaml
+	cleanup
+}
+
+supportbundle_macos_arm64(){
+	echo "Replicated Support Bundle loading for macOS arm64..."
+	wget -q -P /tmp $url/$current_version/support-bundle_darwin_arm64.tar.gz
+	tar zxf /tmp/support-bundle_darwin_arm64.tar.gz -C /tmp	
+	cp $supportbundle_yaml /tmp/support-bundle.yaml
+	/tmp/support-bundle /tmp/support-bundle.yaml
+	cleanup
+}
+
+supportbundle_macos_x86_64(){
+	echo "Replicated Support Bundle loading for macOS x86_64..."
+	wget -q -P /tmp $url/$current_version/support-bundle_darwin_amd64.tar.gz
+	tar zxf /tmp/support-bundle_darwin_amd64.tar.gz -C /tmp
+	cp $supportbundle_yaml /tmp/support-bundle.yaml
+	/tmp/support-bundle /tmp/support-bundle.yaml
+	cleanup
+}
+
+
+supportbundle(){
+	if [ "$os" == "Linux" ] && [ "$arch" == "x86_64" ]
+	then
+		supportbundle_linux_os_x86_64
+	elif [ "$os" == "Linux" ] && ([ "$arch" == "arm64"] || [ "$arch" == "aarch64" ])
+	then
+		supportbundle_linux_os_arm64
+	elif [ "$os" == "Darwin" ] && ([ "$arch" == "arm64" ] || [ "$arch" == "aarch64" ])
+	then
+		supportbundle_macos_arm64
+	elif [ "$os" == "Darwin" ] && [ "$arch" == "x86_64" ]
+	then
+		supportbundle_macos_x86_64
+	fi
+}
+
+supportbundle
