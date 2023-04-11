7bc893a090b6fd991ce7031ec7c473d4b45a4223
Cristhian Guzman <crguzman@jamasoftware.com>
2023-04-11 15:53:30 -0600

    Versioning
    Versioning

---
+++ airgap/v0.0.1/mac/airgap-application-server_mac.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+os=$(uname -s)
+arch=$(uname -m)
+
+current_version="_0.0.1"
+
+cleanup(){
+	if [ -f /tmp/LICENSE ]
+	then
+		rm /tmp/LICENSE
+	fi
+	if [ -f /tmp/README.md ]
+	then
+		rm /tmp/README.md
+	fi
+	if [ -f /tmp/key.pub ]
+	then
+		rm /tmp/key.pub
+	fi
+	if [ -f /tmp/troubleshoot-sbom.tgz ]
+	then
+		rm /tmp/troubleshoot-sbom.tgz*
+	fi
+	if [ -f /tmp/preflight ]
+	then
+		rm /tmp/preflight*
+	fi
+	if [ -f /tmp/support-bundle ]
+	then
+		rm /tmp/support-bundle*
+	fi
+	if [ -f /tmp/airgap-host-preflight_mac ]
+	then
+		rm /tmp/airgap-host-preflight_mac*
+	fi
+
+}
+
+
+supportbundle_macos_arm64(){
+	echo "Analyzing requirments for Jama KOTS on macOS arm64..."
+	tar zxf airgap-host-preflight_mac$current_version.tar.gz -C /tmp
+	tar zxf /tmp/airgap-host-preflight_mac/support-bundle/support-bundle_darwin_arm64.tar.gz -C /tmp
+	/tmp/support-bundle /tmp/airgap-host-preflight_mac/host-preflight$current_version.yml
+	cleanup
+}
+
+supportbundle_macos_x86_64(){
+	echo "Analyzing requirments for Jama KOTS on macOS x86_64..."
+	tar zxf airgap-host-preflight_mac$current_version.tar.gz -C /tmp
+	tar zxf /tmp/airgap-host-preflight_mac/support-bundle/support-bundle_darwin_amd64.tar.gz -C /tmp
+	/tmp/support-bundle /tmp/airgap-host-preflight_mac/host-preflight$current_version.yml
+	cleanup
+}
+
+
+supportbundle(){
+	if [ "$os" == "Linux" ] && [ "$arch" == "x86_64" ]
+	then
+		supportbundle_linux_os_x86_64
+	elif [ "$os" == "Linux" ] && ([ "$arch" == "arm64"] || [ "$arch" == "aarch64" ])
+	then
+		supportbundle_linux_os_arm64
+	elif [ "$os" == "Darwin" ] && ([ "$arch" == "arm64" ] || [ "$arch" == "aarch64" ])
+	then
+		supportbundle_macos_arm64
+	elif [ "$os" == "Darwin" ] && [ "$arch" == "x86_64" ]
+	then
+		supportbundle_macos_x86_64
+	fi
+}
+
+supportbundle
